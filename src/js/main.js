6af6743e4aa161876302f136ed1111a69615d01c
Ginés de Gea <gines@example.com>
2024-01-14 19:03:00 +0100

    Trabajo terminado

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,9 @@
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
+// simple-slider
 import { getSlider } from 'simple-slider';
 getSlider();
+
+// Importa los iconos que necesitas
+import { faHome, faUser, faEnvelope } from "@fortawesome/fontawesome-free";
